0ce9f23a6f66e951f1d225a0bf9e0393f2c06139
Mesbah <ts@tss.com>
2019-09-11 22:48:33 +0200

    Added new table create code in original table installer script.

--- migrations/m150720_174011_initial.php
+++ migrations/m150720_174011_initial.php
@@ -17,6 +17,29 @@ class m150720_174011_initial extends Migration
             'parent_folder_id' => 'int(11) NULL',
             'title' => 'varchar(255) NOT NULL'
         ), '');
+
+        $this->createTable('onlinedrives_app_detail', array(
+            'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+            'space_id' => 'varchar(255) NOT NULL',
+            'user_id' => 'varchar(255) NOT NULL',
+            'email' => 'varchar(255) NOT NULL',
+            'drive_name' => 'varchar(255) NOT NULL',
+            'app_user_id' => 'varchar(255) NOT NULL',
+            'app_password' => 'varchar(255) NOT NULL',
+            'create_date' => 'int(11) NOT NULL',
+            'if_shared' => 'varchar(1) NOT NULL DEFAULT \'N\'',
+            'PRIMARY KEY (`app_user_id`),
+  KEY `id` (`id`)'
+        ), '');
+
+        $this->createTable('onlinedrives_app_drive_path_detail', array(
+            'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+            'drive_path' => 'varchar(255) DEFAULT NULL',
+            'permission' => 'varchar(255) DEFAULT NULL',
+            'onlinedrives_app_detail_id' => 'int(11) NOT NULL',
+            'drive_key' => 'varchar(255) NOT NULL',
+            'PRIMARY KEY (`drive_key`),  KEY `id` (`id`)'
+        ), '');
     }
 
     public function down()
